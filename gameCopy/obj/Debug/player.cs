77ff75d7794633bb6f4d35925733e86022d1c4f3
Bisma Fajar <120157054+bismafajar816@users.noreply.github.com>
2023-04-16 23:12:48 +0500

    Add files via upload

---
+++ gameCopy/obj/Debug/player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gameCopy
+{
+    class player
+    {
+        public int HeroX;
+        public int HeroY;
+        public int score;
+        public int Herohealth;
+        public int enemyX;
+        public int enemyY;
+        public int Enemyhealth;
+    }
+}
